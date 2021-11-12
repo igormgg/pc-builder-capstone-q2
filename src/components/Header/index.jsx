e3fedeae3e5bc179edf1a5f11f3f5555356a98fb
Igor <igormgg@hotmail.com>
2021-11-11 21:58:02 -0300

    feat: logout buton when token exists

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,16 @@ import Logo from "../../assets/images/logo.png";
 import { FaShoppingCart } from "react-icons/fa";
 import { HeaderContainer } from "./styles";
 import { useHistory } from "react-router";
+import { useAuth } from "../../providers/auth";
 
 const Header = ({ button1, button2, cart = true }) => {
   const history = useHistory();
 
+  const { signOut, token } = useAuth();
+
   const handleClick1 = () => {
     if (button1 === "Login") {
-      history.push("sign");
+      history.push("/sign");
     }
     if (button1 === "Produtos") {
       history.push("/");
@@ -17,10 +20,10 @@ const Header = ({ button1, button2, cart = true }) => {
 
   const handleClick2 = () => {
     if (button2 === "Registro") {
-      history.push("sign");
+      token ? signOut() : history.push("/sign");
     }
     if (button2 === "Monte seu PC") {
-      history.push("build");
+      history.push("/build");
     }
   };
 
@@ -29,14 +32,16 @@ const Header = ({ button1, button2, cart = true }) => {
       <img src={Logo} alt="Logo" onClick={() => history.push("/")} />
       <div id="headerEnd">
         <div id="buttonsDiv">
-          <button id="button1" onClick={handleClick1}>
-            {button1}
-          </button>
+          {!token && (
+            <button id="button1" onClick={handleClick1}>
+              {button1}
+            </button>
+          )}
           <button id="button2" onClick={handleClick2}>
-            {button2}
+            {token ? "Logout" : button2}
           </button>
         </div>
-        {cart && <FaShoppingCart onClick={() => history.push("cart")} />}
+        {cart && <FaShoppingCart onClick={() => history.push("/cart")} />}
       </div>
     </HeaderContainer>
   );
