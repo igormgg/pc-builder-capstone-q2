9790408170b8235969624ec34e9205628268e5f7
Igor <igormgg@hotmail.com>
2021-11-16 12:03:17 -0300

    feat: modal ready to be called

--- src/components/ModalContacts/styles.js
+++ src/components/ModalContacts/styles.js
@@ -16,6 +16,114 @@ export const ModalContactsContainer = styled.div`
     animation: modalAnimation 1s 1 ease;
   }
 
+  #contactModal {
+    width: 250px;
+    height: 400px;
+    background-color: var(--gray4);
+    box-shadow: 0 8px 32px 5px black;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    svg {
+      align-self: flex-end;
+      margin: 10px 5% 10px 0;
+      font-size: 24px;
+
+      :hover {
+        cursor: pointer;
+        filter: brightness(0.8);
+      }
+    }
+
+    #profileDiv {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      padding: 0px 20px 0px 20px;
+
+      .devProfile {
+        width: calc(100% / 2);
+        max-width: 205px;
+        margin-bottom: 30px;
+
+        img {
+          width: 60%;
+          max-width: 110px;
+        }
+
+        h2 {
+          font-size: 14px;
+          font-weight: bold;
+        }
+
+        .devSocial {
+          display: flex;
+          justify-content: center;
+          gap: 10px;
+
+          a {
+            width: 30px;
+            height: 30px;
+          }
+
+          img {
+            width: 100%;
+            max-width: 50px;
+
+            :hover {
+              filter: brightness(0.9);
+            }
+          }
+        }
+      }
+    }
+
+    @media screen and (min-width: 320px) {
+      width: 300px;
+      height: 450px;
+
+      #profileDiv {
+        .devProfile {
+          h2 {
+            font-size: 16px;
+          }
+
+          .devSocial {
+            a {
+              width: 50px;
+              height: 50px;
+            }
+          }
+        }
+      }
+    }
+
+    @media screen and (min-width: 700px) {
+      width: 650px;
+      height: 650px;
+
+      svg {
+        margin-top: unset;
+        font-size: 36px;
+      }
+
+      #profileDiv {
+        padding: 20px 50px 20px 50px;
+
+        .devProfile {
+          h2 {
+            font-size: 24px;
+          }
+        }
+      }
+    }
+  }
+
   @keyframes modalAnimation {
     from {
       opacity: 0;
