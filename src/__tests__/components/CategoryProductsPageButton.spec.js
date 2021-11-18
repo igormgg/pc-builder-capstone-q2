2a7511bb6284cdd7af25c088bab158f2384a047b
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2021-11-18 02:37:39 -0300

    feat: buttons in category products page tested

--- src/__tests__/components/CategoryProductsPageButton.spec.js
+++ src/__tests__/components/CategoryProductsPageButton.spec.js
@@ -1,28 +1,53 @@
 import React from "react"
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { AuthProvider } from "../../providers/auth"
 import { BuildProvider } from "../../providers/build"
 import { ModalProvider } from "../../providers/modal"
 import CategoryProducts from "../../pages/CategoryProducts"
+import { MemoryRouter, Route } from "react-router-dom";
+import api from "../../services/api"
+import MockAdapter from "axios-mock-adapter"
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: () => ({
-        category: 'cpu',
-    }),
-}));
+const apiMock = new MockAdapter(api)
 
-describe("Build pages buttons", () => {
-    test("should be able to render the buttons", () => {
+describe("Category products page buttons", () => {
+    test("should be able to render the button", async () => {
+        window.scrollTo = jest.fn()
+        apiMock.onGet("/products").replyOnce(200, {
+            "cpu": [{
+                "model": "Processador AMD Ryzen 5 3600, AM4, 3.6GHz",
+                "img": "https://static.meupc.net/produto/processador-amd-ryzen-5-3600-100100000031box-jXdm89-L.jpg",
+                "price": 1678.31,
+                "description": [
+                    "Marca: AMD",
+                    "Modelo: Ryzen 5 3600",
+                    "Cores: 6",
+                    "Threads: 12",
+                    "Socket: AM4",
+                    "Base Clock: 3.6",
+                    "Cooler Box: Incluso",
+                    "GPU Integrada: Não",
+                    "Consumo: 65 Watts"
+                ]
+            }]
+        })
         render(
             <ModalProvider>
                 <AuthProvider>
                     <BuildProvider >
-                        <CategoryProducts />
+                        <MemoryRouter initialEntries={["/build/cpu"]} >
+                            <Route path="/build/:category" >
+                                <CategoryProducts />
+                            </Route>
+                        </MemoryRouter>
                     </BuildProvider>
                 </AuthProvider>
             </ModalProvider>
         )
-        expect(screen.getAllByText("Adicionar")[0]).toBeTruthy()
+
+        await waitFor(() => {
+            expect(screen.getAllByText("Adicionar")[0]).toBeTruthy()
+        })
+
     })
 })
