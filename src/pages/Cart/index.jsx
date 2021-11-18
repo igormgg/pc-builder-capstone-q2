d63350a8c2dc31ca1034c62baa1d4789195944a4
Igor <igormgg@hotmail.com>
2021-11-18 14:11:52 -0300

    fix: code refactored to clean code

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,25 +1,22 @@
 import api from "../../services/api";
+import CartWithoutProducts from "../../components/CartWithoutProducts";
+import CartWithProducts from "../../components/CartWithProducts";
 import Header from "../../components/Header";
 import Modal from "../../components/Modal";
 import ModalDetails from "../../components/ModalDetails";
-import { AiOutlinePlusCircle } from "react-icons/ai";
 import { CartContainer } from "./styles";
-import { FaTrash } from "react-icons/fa";
-import { Redirect, useHistory } from "react-router";
-import { toast } from "react-toastify";
+import { Redirect } from "react-router";
 import { useAuth } from "../../providers/auth";
-import { useEffect } from "react";
+import { useCart } from "../../providers/cart";
 import { useModal } from "../../providers/modal";
-import { useState } from "react";
+import { useEffect } from "react";
 
 const Cart = () => {
-  const { isOpen, handleOpenModal } = useModal();
+  const { isOpen } = useModal();
 
-  const [cart, setCart] = useState([]);
+  const { cart, clearCart, setCart } = useCart();
 
-  const { setCheckoutAuth, token } = useAuth();
-
-  const history = useHistory();
+  const { token } = useAuth();
 
   const userID = localStorage.getItem("userID") || "";
 
@@ -35,32 +32,6 @@ const Cart = () => {
       })
       .catch((err) => console.log(err));
   }, [cart]);
-
-  const removeFromCart = (item) => {
-    api
-      .delete(`/cart/${item.id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(() => toast.success(`${item.model} foi removido do carrinho!`));
-  };
-
-  const clearCart = () => {
-    cart.forEach((item) => {
-      api.delete(`/cart/${item.id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    });
-    toast.success(`Todos os produtos foram removidos do carrinho.`);
-  };
-
-  const checkoutClickHandler = () => {
-    setCheckoutAuth(true);
-    history.push("/checkout");
-  };
 
   if (!token) {
     return <Redirect to="/sign" />;
@@ -84,45 +55,7 @@ const Cart = () => {
             Remover Todos
           </button>
         </div>
-        {cart.length === 0 ? (
-          <>
-            <h1 id='noProductsTitle'>Não há produtos no carrinho</h1>
-            <button id="backToShopping" onClick={() => history.push("/")}>
-              Voltar às compras
-            </button>
-          </>
-        ) : (
-          <>
-            <ul id="productsContainer">
-              {cart.map((item, index) => (
-                <li key={index}>
-                  <div id="leftDiv">
-                    <img src={item.img} alt={item.model} />
-                    <div id="productDetails">
-                      <h3>{item.model}</h3>
-                      <p onClick={() => handleOpenModal(item)}>
-                        Exibir detalhes <AiOutlinePlusCircle />
-                      </p>
-                      <span>{(item.price).toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}</span>
-                    </div>
-                  </div>
-                  <div id="trashDiv">
-                    <FaTrash onClick={() => removeFromCart(item)} />
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <div id="bottomContainer">
-              <h2>Total:</h2>
-              <h2>
-                {cart.reduce((acc, item) => acc + item.price, 0).toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}
-              </h2>
-            </div>
-            <button id="checkoutButton" onClick={checkoutClickHandler}>
-              Checkout
-            </button>
-          </>
-        )}
+        {cart.length === 0 ? <CartWithoutProducts /> : <CartWithProducts />}
       </CartContainer>
     </>
   );
